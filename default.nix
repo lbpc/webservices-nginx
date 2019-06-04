6b3ab5165243c04d162c36ad138dc5e1fd214fbe
sidorov <sidorov@majordomo.ru>
2019-06-04 19:31:06 +0300

    refactored

--- default.nix
+++ default.nix
@@ -1,9 +1,6 @@
 with import <nixpkgs> {
   overlays = [
-    (import (builtins.fetchGit {
-      url = "git@gitlab.intr:_ci/nixpkgs.git";
-      ref = "master";
-    }))
+    (import (builtins.fetchGit { url = "git@gitlab.intr:_ci/nixpkgs.git"; ref = "master"; }))
     (self: super: {
       glibcLocales = super.glibcLocales.override {
         allLocales = false;
@@ -12,75 +9,73 @@ with import <nixpkgs> {
     })
   ];
 };
-with stdenv.lib;
 
 let
   
-inherit (lib) firstNChars flattenSet dockerRunCmd;
-inherit (builtins) getEnv toJSON;
-  
-nginx = stdenv.mkDerivation rec {
-  name = "nginx-${version}";
-  version = "1.16.0";
-  src = fetchurl {
-    url = "https://nginx.org/download/nginx-${version}.tar.gz";
-    sha256 = "0i8krbi1pc39myspwlvb8ck969c8207hz84lh3qyg5w7syx7dlsg";
-  };
-  buildInputs = [ openssl zlib pcre nginxLua.inputs ];
-  configureFlags = [
-    "--with-http_ssl_module"
-    "--with-http_realip_module"
-    "--with-http_sub_module"
-    "--with-http_gunzip_module"
-    "--with-http_gzip_static_module"
-    "--with-threads"
-    "--with-pcre-jit"
-    "--with-file-aio"
-    "--with-http_v2_module"
-    "--with-http_stub_status_module"
-    "--without-http_geo_module"
-    "--without-http_empty_gif_module"
-    "--without-http_fastcgi_module"
-    "--without-http_uwsgi_module"
-    "--without-http_scgi_module"
-    "--without-http_grpc_module"
-    "--without-http_memcached_module"
-    "--without-http_charset_module"
-    "--without-select_module"
-    "--without-poll_module"
-    "--user=root"
-    "--error-log-path=/dev/stderr"
-    "--http-log-path=/dev/stdout"
-    "--pid-path=/run/nginx.pid"
-    "--lock-path=/run/nginx.lock"
-    "--http-client-body-temp-path=/run/client_body_temp"
-    "--http-proxy-temp-path=/run/proxy_temp"
-    "--add-module=${nginxLua.src}"
-    "--add-module=${nginxVts.src}"
-    "--add-module=${nginxSysGuard.src}"
-    "--add-module=${nginxModules.develkit.src}"
-  ]; 
-  preConfigure = nginxLua.preConfigure;
-  hardeningEnable = [ "pie" ];
-  enableParallelBuilding = true;
-  postInstall = ''
-    shopt -s extglob
-    mv $out/sbin $out/bin
-    rm -f $out/conf/!(mime.types)
-    rm -rf $out/html
-  '';
+inherit (builtins) concatMap getEnv toJSON;
+inherit (dockerTools) buildLayeredImage;
+inherit (lib) concatMapStringsSep firstNChars flattenSet dockerRunCmd;
+inherit (stdenv) mkDerivation;
+
+nginx = with nginxModules; let modules = [ nginxLua nginxVts nginxSysGuard develkit ]; in
+  mkDerivation rec {
+    name = "nginx-${version}";
+    version = "1.16.0";
+    src = fetchurl {
+      url = "https://nginx.org/download/nginx-${version}.tar.gz";
+      sha256 = "0i8krbi1pc39myspwlvb8ck969c8207hz84lh3qyg5w7syx7dlsg";
+    };
+    buildInputs = [ openssl zlib pcre ] ++ concatMap (mod: mod.inputs or []) modules;
+    configureFlags = [
+      "--with-http_ssl_module"
+      "--with-http_realip_module"
+      "--with-http_sub_module"
+      "--with-http_gunzip_module"
+      "--with-http_gzip_static_module"
+      "--with-threads"
+      "--with-pcre-jit"
+      "--with-file-aio"
+      "--with-http_v2_module"
+      "--with-http_stub_status_module"
+      "--without-http_geo_module"
+      "--without-http_empty_gif_module"
+      "--without-http_fastcgi_module"
+      "--without-http_uwsgi_module"
+      "--without-http_scgi_module"
+      "--without-http_grpc_module"
+      "--without-http_memcached_module"
+      "--without-http_charset_module"
+      "--without-select_module"
+      "--without-poll_module"
+      "--user=root"
+      "--error-log-path=/dev/stderr"
+      "--http-log-path=/dev/stdout"
+      "--pid-path=/run/nginx.pid"
+      "--lock-path=/run/nginx.lock"
+      "--http-client-body-temp-path=/run/client_body_temp"
+      "--http-proxy-temp-path=/run/proxy_temp"
+    ] ++ map (mod: "--add-module=${mod.src}") modules; 
+    preConfigure = (concatMapStringsSep "\n" (mod: mod.preConfigure or "") modules);
+    hardeningEnable = [ "pie" ];
+    enableParallelBuilding = true;
+    postInstall = ''
+      shopt -s extglob
+      mv $out/sbin $out/bin
+      rm -f $out/conf/!(mime.types)
+      rm -rf $out/html
+    '';
 };
 
-nginxConfLayer = stdenv.mkDerivation rec {
+nginxConfLayer = with luajitPackages; mkDerivation rec {
   name = "mj-nginx-config";
   srcs = [ ./lua ./conf ./doc ];
   sourceRoot = ".";
-  luaPackages = [ luaRestyCore luaRestyLrucache lua51Packages.luasocket ];
-  buildInputs = [ nginx mjerrors pandoc ] ++ luaPackages;
-  luaPath = concatMapStringsSep ";" (p: lua51Packages.getLuaPath p) luaPackages;
-  luaCPath = concatMapStringsSep ";" (p: lua51Packages.getLuaCPath p) luaPackages;
+  luaPackages = [ luaRestyCore luaRestyLrucache luasocket ];
+  buildInputs = [ nginx mjHttpErrorPages pandoc ] ++ luaPackages;
+  luaPath = concatMapStringsSep ";" (p: getLuaPath p) luaPackages;
+  luaCPath = concatMapStringsSep ";" (p: getLuaCPath p) luaPackages;
   buildPhase = ''
-    cp -r ${mjerrors}/html .
+    cp -r ${mjHttpErrorPages}/html .
     cp ${nginx}/conf/mime.types conf
     chmod +w html
     mkdir html/doc
@@ -112,11 +107,11 @@ gitAbbrev = firstNChars 8 (getEnv "GIT_COMMIT");
 
 in
 
-pkgs.dockerTools.buildLayeredImage rec {
+buildLayeredImage rec {
   name = "docker-registry.intr/webservices/nginx";
-  tag = if (gitAbbrev != "") then gitAbbrev else "latest";
+  tag = if gitAbbrev != "" then gitAbbrev else "latest";
   maxLayers = 128;
-  contents = [ nginx glibcLocales tzdata nginxConfLayer ];
+  contents = [ nginx nginxConfLayer glibcLocales tzdata ];
   extraCommands=''
     mkdir -p {etc/nginx,usr/share/nginx/html,var/log/nginx}
     echo 'root:x:0:0:root:/run:' > etc/passwd
