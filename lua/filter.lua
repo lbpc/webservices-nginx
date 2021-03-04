8ddd7b2c2f5ce01996fedb75b811ed471cf4aa66
Петр Сидоров <sidorov@majordomo.ru>
2021-03-04 19:44:29 +0300

    filter: do not generate robots.txt, always respond with 503 and Retry-After: ttl when action is setCookie, add human-readable description

--- lua/filter.lua
+++ lua/filter.lua
@@ -27,13 +27,19 @@ if act == 0 then
   if ngx.var.cookie_mj_anti_flood == cookie then
     ngx.var.filter_passed = 1 
     return
-  elseif ngx.var.uri == '/robots.txt' then
-    ngx.status = ngx.HTTP_SERVICE_UNAVAILABLE
-    ngx.say('User-agent: *\nDisallow: /')
-    ngx.exit(ngx.HTTP_OK)
   else
+    local ttl = 1
+	local descr = 'С вашего IP адреса зафиксирован ряд подозрительных запросов.'
+    if protected then
+      ttl = ngx.shared.protection_table:ttl(ngx.var.host)
+	  descr = 'В данный момент сайт находится под защитой от автоматически сгенерированных запросов.'
+    else
+      ttl = ngx.shared.ip_filter_table:get(ngx.var.remote_addr)
+	end
+    ngx.status = ngx.HTTP_SERVICE_UNAVAILABLE
+    ngx.header.Retry_After = math.floor(ttl)
     ngx.header.content_type = 'text/html'
-    ngx.say(string.format([[<html><body onload="document.cookie='mj_anti_flood=%s;'+new Date(Date.now()+604800000).toUTCString();document.location.reload();"></body></html>]], cookie))
+    ngx.say(string.format([[<!DOCTYPE html><html><head><script>document.cookie='mj_anti_flood=%s;'+new Date(Date.now()+604800000).toUTCString();document.location.reload();</script><meta charset="utf-8"></head><body><p>%s Ограничение будет действовать в течение %d с.</p><p>Данная страница была отдана сервером с кодом состояния <b>HTTP 503</b> и заголовком <b>Retry-After: %d</b>, и не должна отображаться в веб-браузерах с поддержкой JavaScript.<noscript>Инструкции по включению JavaScript в популярных браузерах доступны по <a href="https://www.enable-javascript.com/ru/">этой ссылке</a>.</noscript><p>Если вы владелец сайта, дополнительную информацию вы можете узнать по e‑mail <a href="mailto:support@majordomo.ru">support@majordomo.ru</a></p></body></html>]], cookie, descr, ttl, ttl))
     ngx.exit(ngx.HTTP_OK)
   end
 elseif act == 1 then
